00ae599a458a20b7328fdb482c094750ad75c083
luvjke <nik_antonov_555@mail.ru>
2024-09-20 18:47:01 +0400

    fixing5

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import NextAuth from 'next-auth/next';
+import NextAuth from 'next-auth';
 import { authOptions } from '../../../../../shared/constans/auth_options';
-
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST, authOptions };
+
+export { handler as GET, handler as POST };
